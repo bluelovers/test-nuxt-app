4870c977bb886c84b3b40a5432d7b3d28c4c5b99
bluelovers <codelovers@users.sourceforge.net>
2019-06-14 15:40:08 +0800

    test now nuxt ssr

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -14,6 +14,14 @@
           </small>
         </footer>
       </blockquote>
+
+
+      <nuxt-link to="/test-import-some-module">test-import-some-module.vue</nuxt-link>
+
+      <nuxt-link to="/ssr">ssr.vue</nuxt-link>
+
+      <nuxt-link to="/parent/child">parent/child</nuxt-link>
+
     </v-flex>
   </v-layout>
 </template>
---
+++ pages/ssr.vue
@@ -0,0 +1,29 @@
+<template>
+
+	<div>
+
+		<div>{{data}}</div>
+		<div>{{text}}</div>
+
+	</div>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {
+	data()
+	{
+		return {
+			date: Date.now(),
+			text: (new Date).toString(),
+		}
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
