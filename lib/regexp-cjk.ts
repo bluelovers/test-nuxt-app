320f38974598cef8abb2e98b34eb220cb10ffa1f
bluelovers <codelovers@users.sourceforge.net>
2019-06-16 00:01:23 +0800

    .

--- lib/regexp-cjk.ts
+++ lib/regexp-cjk.ts
@@ -2,11 +2,24 @@
  * Created by user on 2019/6/14.
  */
 
-import zhRegExp, { IOptions } from 'regexp-cjk';
-import createZhRegExpPlugin, { IZhRegExpPluginOptions } from 'regexp-cjk-plugin-extra';
+import _zhRegExp, { IOptions } from 'regexp-cjk';
+import createZhRegExpPlugin from 'regexp-cjk-plugin-extra';
+import createZhRegExpCorePlugin from 'regexp-cjk-plugin-escape-unicode-property';
 
-let options: IZhRegExpPluginOptions = {
-	autoDeburr: true,
-};
+export const zhRegExp = _zhRegExp.use({
+	on: [
+		createZhRegExpPlugin({
+			autoVoice: true,
+			autoLocale: true,
+			autoFullHaif: true,
+			autoDeburr: true,
+		}),
+	],
+	onCore: [
+		createZhRegExpCorePlugin({
+			escapeAuto: true,
+		}),
+	],
+});
 
 export default zhRegExp
